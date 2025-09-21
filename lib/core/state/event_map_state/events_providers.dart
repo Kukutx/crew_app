d8f811faedaf3099b2b5f62264255d18323756b1
kukutx <61358650+Kukutx@users.noreply.github.com>
2025-09-22 05:11:42 +0800

    feat(events): poll events provider periodically

--- lib/core/state/event_map_state/events_providers.dart
+++ lib/core/state/event_map_state/events_providers.dart
@@ -1,4 +1,6 @@
 // lib/features/events/presentation/events_providers.dart
+import 'dart:async';
+
 import 'package:crew_app/core/state/di/providers.dart';
 import 'package:crew_app/features/events/data/event.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -6,13 +8,18 @@ import 'package:latlong2/latlong.dart';
 
 
 final eventsProvider =
-    AsyncNotifierProvider<EventsCtrl, List<Event>>(EventsCtrl.new);
+    AutoDisposeAsyncNotifierProvider<EventsCtrl, List<Event>>(EventsCtrl.new);
 
-class EventsCtrl extends AsyncNotifier<List<Event>> {
+class EventsCtrl extends AutoDisposeAsyncNotifier<List<Event>> {
+  Timer? _pollingTimer;
+
   @override
   Future<List<Event>> build() async {
     final api = ref.read(apiServiceProvider);
-    return api.getEvents();
+    final events = await api.getEvents();
+    state = AsyncData(events);
+    _startPolling();
+    return events;
   }
 
   Future<Event> createEvent({
@@ -25,9 +32,24 @@ class EventsCtrl extends AsyncNotifier<List<Event>> {
     final newEv = await api.createEvent(
       title, locationName, description, pos.latitude, pos.longitude,
     );
-    // 追加到列表
-    final curr = state.value ?? const <Event>[];
-    state = AsyncData([...curr, newEv]);
+    await _refreshEvents();
     return newEv;
+  }
+
+  void _startPolling() {
+    _pollingTimer?.cancel();
+    _pollingTimer = Timer.periodic(
+      const Duration(seconds: 30),
+      (_) => unawaited(_refreshEvents()),
+    );
+    ref.onDispose(() {
+      _pollingTimer?.cancel();
+      _pollingTimer = null;
+    });
+  }
+
+  Future<void> _refreshEvents() async {
+    final api = ref.read(apiServiceProvider);
+    state = await AsyncValue.guard(() => api.getEvents());
   }
 }

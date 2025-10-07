17428d90a52d185b9ef927c7b34b09762f05e99f
kukutx <61358650+Kukutx@users.noreply.github.com>
2025-10-07 14:12:48 +0200

    Use AppMasonryGrid for user profile events

--- lib/features/events/presentation/list/events_list_page.dart
+++ lib/features/events/presentation/list/events_list_page.dart
@@ -1,6 +1,5 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:crew_app/features/events/data/event.dart';
-import 'package:crew_app/features/events/presentation/widgets/event_image_placeholder.dart';
+import 'package:crew_app/features/events/presentation/widgets/event_grid_card.dart';
 import 'package:crew_app/l10n/generated/app_localizations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -9,7 +8,6 @@ import 'package:crew_app/shared/widgets/app_masonry_grid.dart';
 import '../../../../core/state/app/app_overlay_provider.dart';
 import '../../../../core/error/api_exception.dart';
 import 'package:crew_app/features/events/state/events_providers.dart';
-import '../detail/events_detail_page.dart';
 
 class EventsListPage extends ConsumerStatefulWidget {
   const EventsListPage({super.key});
@@ -54,9 +52,9 @@ class _EventsListPageState extends ConsumerState<EventsListPage> {
               crossAxisSpacing: 8,
               itemCount: events.length,
               physics: const AlwaysScrollableScrollPhysics(),
-              itemBuilder: (context, i) => EventGridItem(
+              itemBuilder: (context, i) => EventGridCard(
                 event: events[i],
-                index: i,
+                heroTag: 'event_$i',
                 onShowOnMap: (event) {
                   ref.read(appOverlayIndexProvider.notifier).state = 1;
                   ref.read(mapFocusEventProvider.notifier).state = event;
@@ -104,82 +102,3 @@ class _CenteredScrollable extends StatelessWidget {
   }
 }
 
-// Flutter idiomatic 的 GridItem
-class EventGridItem extends StatelessWidget {
-  final Event event;
-  final int index;
-  final ValueChanged<Event>? onShowOnMap;
-
-  const EventGridItem({
-    required this.event,
-    required this.index,
-    this.onShowOnMap,
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    final mq = MediaQuery.of(context);
-    final memCacheW = ((mq.size.width / 2) * mq.devicePixelRatio).round();
-    final heroTag = 'event_$index';
-     // Tips： 判断imageUrls是否有值，否则用coverImageUrl
-    // (这是当前后端的问题，因为目前后端只有在创建的时候才会自动赋值coverImageUrl，而用SeedDataService预先插入的数据没用自动首页逻辑)，日后待看获取直接用event.coverImageUrl
-    final imageUrl = event.firstAvailableImageUrl;
-
-    return Material(
-      elevation: 4,
-      borderRadius: BorderRadius.circular(12),
-      clipBehavior: Clip.antiAlias,
-      child: InkWell(
-        onTap: () async {
-          final result = await Navigator.push<Event>(
-            context,
-            MaterialPageRoute(
-              builder: (_) => EventDetailPage(event: event),
-            ),
-          );
-          if (result != null) {
-            onShowOnMap?.call(result);
-          }
-        },
-        child: Stack(
-          children: [
-            Hero(
-              tag: heroTag,
-              child: imageUrl != null
-                  ? CachedNetworkImage(
-                      imageUrl: imageUrl,
-                      fit: BoxFit.cover,
-                      memCacheWidth: memCacheW,
-                      placeholder: (c, _) => const AspectRatio(
-                        aspectRatio: 1,
-                        child: Center(
-                          child: CircularProgressIndicator(strokeWidth: 2),
-                        ),
-                      ),
-                      errorWidget: (c, _, _) =>
-                          const EventImagePlaceholder(aspectRatio: 1),
-                    )
-                  : const EventImagePlaceholder(aspectRatio: 1),
-            ),
-            Positioned(
-              left: 0,
-              right: 0,
-              bottom: 0,
-              child: Container(
-                color: Colors.black54,
-                padding: const EdgeInsets.all(6),
-                child: Text(
-                  event.title,
-                  maxLines: 1,
-                  overflow: TextOverflow.ellipsis,
-                  style: const TextStyle(color: Colors.white),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}

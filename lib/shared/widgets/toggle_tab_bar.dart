1e914a1eac1c52662431784cd5db1523e24c5301
kukutx <61358650+Kukutx@users.noreply.github.com>
2025-10-11 01:29:06 +0200

    Integrate plaza actions into toggle tab bar

--- lib/shared/widgets/toggle_tab_bar.dart
+++ lib/shared/widgets/toggle_tab_bar.dart
@@ -12,6 +12,7 @@ class ToggleTabBar extends StatelessWidget {
     required this.onChanged,
     this.firstIcon = Icons.favorite,
     this.secondIcon = Icons.autorenew,
+    this.accessoryBuilder,
   });
 
   final int selectedIndex;
@@ -20,25 +21,38 @@ class ToggleTabBar extends StatelessWidget {
   final ToggleTabChanged onChanged;
   final IconData firstIcon;
   final IconData secondIcon;
+  final Widget? Function(BuildContext context, int selectedIndex)?
+      accessoryBuilder;
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
+    final accessory = accessoryBuilder?.call(context, selectedIndex);
+
+    return Column(
+      mainAxisSize: MainAxisSize.min,
       children: [
-        ToggleTabChip(
-          label: firstLabel,
-          icon: firstIcon,
-          selected: selectedIndex == 0,
-          onTap: () => onChanged(0),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ToggleTabChip(
+              label: firstLabel,
+              icon: firstIcon,
+              selected: selectedIndex == 0,
+              onTap: () => onChanged(0),
+            ),
+            const SizedBox(width: 12),
+            ToggleTabChip(
+              label: secondLabel,
+              icon: secondIcon,
+              selected: selectedIndex == 1,
+              onTap: () => onChanged(1),
+            ),
+          ],
         ),
-        const SizedBox(width: 12),
-        ToggleTabChip(
-          label: secondLabel,
-          icon: secondIcon,
-          selected: selectedIndex == 1,
-          onTap: () => onChanged(1),
-        ),
+        if (accessory != null) ...[
+          const SizedBox(height: 8),
+          accessory,
+        ],
       ],
     );
   }

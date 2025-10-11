1bd10be1d01320f24beb1f8cb0b7ea701bda4fad
Kukutx <liuzhongli.ascii@gmail.com>
2025-10-11 21:12:31 +0200

    Update Code

--- lib/l10n/generated/app_localizations_en.dart
+++ lib/l10n/generated/app_localizations_en.dart
@@ -280,12 +280,10 @@ class AppLocalizationsEn extends AppLocalizations {
   String get map_location_info_nearby_title => 'Nearby places';
 
   @override
-  String get map_location_info_nearby_empty =>
-      'No places found within 100 m.';
+  String get map_location_info_nearby_empty => 'No places found within 100 m.';
 
   @override
-  String get map_location_info_nearby_error =>
-      'Unable to load nearby places.';
+  String get map_location_info_nearby_error => 'Unable to load nearby places.';
 
   @override
   String get map_place_details_title => 'Place details';
